19c8f0aeca4ea16761253b2257a6da4aae96d150
Omar Rakib <orakib@e2r6p4.1337.ma>
2023-02-07 18:43:01 +0100

    parsing

--- parsing.c
+++ parsing.c
@@ -6,51 +6,67 @@
 /*   By: orakib <orakib@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 16:38:54 by orakib            #+#    #+#             */
-/*   Updated: 2023/02/06 16:09:50 by orakib           ###   ########.fr       */
+/*   Updated: 2023/02/07 16:29:21 by orakib           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	main(int ac, char **av)
+int	get_fd(int ac, char **av)
+{
+	int	fd;
+
+	if (ac != 2)
+	{
+		write (2, "Error\nWrong number of arguments\n", 33);
+		exit(EXIT_FAILURE);
+	}
+	if (!check_ext(av[1]))
+	{
+		write (2, "Error\nWrong extension\n", 23);
+		exit(EXIT_FAILURE);
+	}
+	fd = open(av[1], O_RDONLY);
+	if (fd == -1)
+	{
+		perror("Error\n");
+		exit(errno);
+	}
+	return (fd);
+}
+
+// char	*maptostr(fd)
+// {
+	
+// }
+
+void	map_parsing(int ac, char **av)
 {
 	int		fd;
 	char	*str;
 	char	*line;
 	char	**split;
 
-	if (ac != 2)
-		return (0);
-	if (!check_ext(av[1]))
-		return (0);
-	fd = open(av[1], O_RDONLY);
+	fd = get_fd(ac, av);
 	str = get_next_line(fd);
-	if (!str)
-		return (0);
 	while (str[ft_strlen(str) - 1] == '\n')
 	{
 		line = get_next_line(fd);
 		if (!line)
-		{
-			str = ft_strjoin2(str, "\n");
 			break ;
-		}
 		str = ft_strjoin2(str, line);
 		free(line);
 	}
-	if (str[0] == '\n' || str[ft_strlen(str) - 1] == '\n')
-	{
-		printf("Error\n");
-		return (0);
-	}
+	// if (str[0] == '\n' || str[ft_strlen(str) - 1] == '\n')
+	// {
+	// 	printf("Error\n");
+	// 	exit(EXIT_FAILURE);
+	// }
 	printf("%s", str);
-	printf ("\n\n");
+	// printf ("\n\n");
 	split = ft_split(str, '\n');
 	// free(str);
 	int i = -1;
-	while (split[++i])
-		printf("%s\n", split[i]);
-	i = -1;
 	while (split[++i + 1])
 		if (ft_strlen(split[i]) != ft_strlen(split[i + 1]))
 		{
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: orakib <orakib@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 18:07:38 by orakib            #+#    #+#             */
-/*   Updated: 2023/02/05 18:50:58 by orakib           ###   ########.fr       */
+/*   Updated: 2023/02/07 12:53:07 by orakib           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,13 @@
 # include <stdlib.h>
 # include <math.h>
 # include <fcntl.h>
+# include <errno.h>
 # include "gnl/get_next_line.h"
 # include "MLX42/MLX42.h"
 
 int		ft_strlen(char *s);
 int		check_ext(char *str);
 char	**ft_split(char *s, char c);
+void	map_parsing(int ac, char **av);
 
 #endif
